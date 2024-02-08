5cf79cf599265077e51042b4b8db2aac12c9a22d
JONATHANJ64 <126198122+JONATHANJ64@users.noreply.github.com>
2024-02-08 05:43:34 -0800

    Requirement B1.   Install the Landon Hotel scheduling application in your integrated development environment (IDE). Modify the Java classes of application to display a welcome message.

--- src/main/UI/src/app/app.component.ts
+++ src/main/UI/src/app/app.component.ts
@@ -14,6 +14,9 @@ import {map} from "rxjs/operators";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+//Create Observables - Not Null
+  welcomeMessageEng$!: Observable<string>
+  welcomeMessageFre$!: Observable<string>
 
   constructor(private httpClient:HttpClient){}
 
@@ -29,6 +32,14 @@ export class AppComponent implements OnInit{
   currentCheckOutVal!:string;
 
     ngOnInit(){
+      // Call welcome message in different languages to site
+      //English message.  localhost:8080/welcome/?lang=en-us
+      this.welcomeMessageEng$ = this.httpClient.get(this.baseURL + "/welcome/?lang=en-US", {responseType: "text"})
+
+      //French Message. localhost:8080/welcome/?lang=fr-CA
+      this.welcomeMessageFre$ = this.httpClient.get(this.baseURL + "/welcome/?lang=fr-CA", {responseType: "text"})
+
+
       this.roomsearch= new FormGroup({
         checkin: new FormControl(' '),
         checkout: new FormControl(' ')
